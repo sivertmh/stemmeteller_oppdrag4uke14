58a453dccf85a133efecbb4a588bfcf574f85f5d
Sivert Mathisen Hansen <sivert.mathisen.hansen@gmail.com>
2025-04-09 11:54:08 +0200

    ui/ux changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-// AP
+// Generell funskjon for stemmeknapp
+function stem(stemParti, stemmerAntall){
+    stemmerAntall++;
+    stemParti.textContent = stemmerAntall;
+    localStorage.setItem(stemmerAntall, stemmerAntall);
+    oppdaterDiagram();
+}
+
+// Ap
 let StemAp = document.getElementById("stemAp");
 let ApStemmer = localStorage.getItem("ApStemmer") ? parseInt(localStorage.getItem("ApStemmer")) : 0;
 StemAp.textContent = ApStemmer;
@@ -15,22 +23,34 @@ let StemHr = document.getElementById("stemHoyre");
 let HrStemmer = localStorage.getItem("HrStemmer") ? parseInt(localStorage.getItem("HrStemmer")) : 0;
 StemHr.textContent = HrStemmer;
 
-// Funksjon som øker antall stemmer med 1 hver gang man trykker på "Stem Høyre"-knappen
 function stemHoyre(){
-    HrStemmer++;
-    StemHr.textContent = HrStemmer;
-    localStorage.setItem("HrStemmer", HrStemmer);
-    oppdaterDiagram();
+  HrStemmer++;
+  StemHr.textContent = HrStemmer;
+  localStorage.setItem("HrStemmer", HrStemmer);
+  oppdaterDiagram();
+}
+
+// Sp
+let StemSp = document.getElementById("stemSp");
+let antallSpStemmer = localStorage.getItem("antallSpStemmer") ? parseInt(localStorage.getItem("antallSpStemmer")) : 0;
+stemSp.textContent = antallSpStemmer;
+
+function stemSp(){
+  antallSpStemmer++;
+  StemSp.textContent = antallSpStemmer;
+  localStorage.setItem("antallSpStemmer", antallSpStemmer);
+  oppdaterDiagram();
 }
 
 // Diagram
 
 function oppdaterDiagram () {
-  var xValues = ["Arbeiderpartiet", "Høyre"];
-var yValues = [ApStemmer, HrStemmer];
+  var xValues = ["Arbeiderpartiet", "Høyre", "Senterpartiet"];
+var yValues = [ApStemmer, HrStemmer, antallSpStemmer];
 var barColors = [
   "#ef3340",
   "#086af1",
+  "#00843d",
 ];
 
 new Chart("valgResultat", {
